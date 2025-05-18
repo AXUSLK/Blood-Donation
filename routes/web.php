c8d837f5a459da461a3ab9485144bd1d3b3fd61a
AXUSLK <thilina.lyceum@gmail.com>
2025-05-18 23:27:51 +0530

    🛂 Authentication Added (Laravel Breeze and Spatie)

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,34 @@
 <?php
 
 use App\Http\Controllers\Backend\DashboardController as BackendDashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->name('backend.')->group(function () {
-    // Route::group(['middleware' => ['auth', 'verified']], function () {
-    // Admin
-    Route::name('admin.')->group(function () {
-        Route::get('/dashboard', [BackendDashboardController::class, 'adminDashboard'])->name('dashboard');
-    });
-    // Donor
-    Route::name('donor.')->group(function () {
-        Route::get('/dashboard2', [BackendDashboardController::class, 'donorDashboard'])->name('dashboard');
-    });
-    // });
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('admin')->name('backend.')->group(function () {
+    Route::group(['middleware' => ['auth', 'verified']], function () {
+        // Admin
+        Route::name('admin.')->group(function () {
+            Route::get('/dashboard', [BackendDashboardController::class, 'adminDashboard'])->name('dashboard');
+        });
+        // Donor
+        Route::name('donor.')->group(function () {
+            Route::get('/dashboard2', [BackendDashboardController::class, 'donorDashboard'])->name('dashboard');
+        });
+    });
+});
+
+require __DIR__ . '/auth.php';
